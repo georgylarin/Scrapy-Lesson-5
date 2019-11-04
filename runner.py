66697dcf37b177f164e53d3f68c64b46effd9a98
georgylarin <mmklarin@yandex.ru>
2019-11-04 15:50:32 +0300

    В runner поменял Settings на settings в абтрибутах setmodule паука

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ from jobparser.spiders.hhru import HhruSpider
 
 if __name__ == '__main__':
     crawler_settings = Settings()
-    crawler_settings.setmodule(Settings)
+    crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
     process.crawl(HhruSpider)
     process.crawl(SuperjobRuSpider)
